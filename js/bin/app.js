315b7c0fdedfdb09b4e89b73201bef5638788011
Sprazzeus <sprazzeus@gmail.com>
2016-07-18 14:54:08 +0800

    testing ajax request

--- js/bin/app.js
+++ js/bin/app.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
   $.ajax({
-    url: 'https://project3pockety.herokuapp.com/allresources',
+    url: 'http://project3pockety.herokuapp.com/allresources',
     success: function (data) {
-      $.each(data.resource, function (index, item) {
-        $('#cards').append('<div class="col s6"><div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="images/work3.jpg"></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">Date from InstaParser goes here</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span><p>Here is the summary to be grabbed from Instaparser on the server side.</p></div><div class="card-action"><a href="' + item.url + '">Go to original</a></div></div></div>'
-        )
-      })
+      $('#cards').append('<div>HI</div>')
+      // $.each(data, function (index, item) {
+      //   $('#cards').append('<div class="col s6"><div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="images/work3.jpg"></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">Date from InstaParser goes here</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span><p>Here is the summary to be grabbed from Instaparser on the server side.</p></div><div class="card-action"><a href="' + item.url + '">Go to original</a></div></div></div>'
+      //   )
+      // })
     }
 
   })
