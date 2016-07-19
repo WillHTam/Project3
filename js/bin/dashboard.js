960ea596ee60907cd625e5bea9b059fa89305d20
Sprazzeus <sprazzeus@gmail.com>
2016-07-19 10:19:08 +0800

    implemented auth tokens

--- js/bin/dashboard.js
+++ js/bin/dashboard.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+console.log(window.localStorage.email + window.localStorage.auth_token)
 $('.modal-trigger').leanModal();
 
 // populate resource cards
--- js/bin/register.js
+++ js/bin/register.js
@@ -2,31 +2,27 @@ var serverURL = 'https://project3pockety.herokuapp.com/'
 
 $(function () {
   // listen for the form login
-  $('#login-form').on('submit', function (event) {
+  $('#register-form').on('submit', function (event) {
     event.preventDefault()
     var formData = $(this).serialize()
-    login(formData)
+    signin(formData)
   })
 })
 
-function login (formData) {
+function signin (formData) {
   $.ajax({
     type: 'POST',
-    url: serverURL + 'login',
+    url: serverURL + 'register',
     data: formData,
     success: function (response) {
-      // success save the repsonse
-      window.localStorage.email = $('#email').val()
-      window.localStorage.auth_token = response.auth_token
-      console.log(response.auth_token)
-      // then redirect
-      window.location.href = 'dashboard.html'
+      // then redirect to login page
+      window.location.href = 'login.html'
     },
     error: function (xhr, ajaxOptions, thrownError) {
       // else output error
       console.log(xhr.status)
       console.log(thrownError)
-      window.alert('Login Failed')
+      window.alert('Registration Failed')
     }
   })
 }
