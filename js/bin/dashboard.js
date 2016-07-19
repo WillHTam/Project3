afdce93181af8045b2cf1dbe288d06b3f34adeec
Sprazzeus <sprazzeus@gmail.com>
2016-07-19 15:30:11 +0800

    UI: added masonry, tabs, card tags, favicon. Schema updated: source site_name, summary. Misc: removed extraneous user_resources.js.
    Resources are loaded from server according to current user.

--- js/bin/dashboard.js
+++ js/bin/dashboard.js
@@ -1,18 +1,63 @@
 $(document).ready(function() {
-console.log(window.localStorage.email + window.localStorage.auth_token)
-$('.modal-trigger').leanModal();
 
-// populate resource cards
+  var serverURL = 'https://project3pockety.herokuapp.com/'
+  var currentUser = null
 
-  $.ajax({
-    url: 'https://project3pockety.herokuapp.com/allresources',
-    success: function (data) {
-      $.each(data, function (index, item) {
-        $('#cards').append('<div class="col s6"><div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="images/work3.jpg"></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">Date from InstaParser goes here</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span><p>Here is the summary to be grabbed from Instaparser on the server side.</p></div><div class="card-action"><a href="' + item.url + '">Go to original</a></div></div></div>'
+  // We are assuming Local Storage is supported
+  $(function () {
+    if (!window.localStorage['email'] || !window.localStorage['auth_token']) window.location.href = 'login.html'
+    else loadResources()
+  })
+
+  console.log(window.localStorage.email + window.localStorage.auth_token)
+  $('.modal-trigger').leanModal();
+
+  // populate resource cards
+  function loadResources () {
+    $.ajax({
+      url: 'https://project3pockety.herokuapp.com/allresources',
+      success: function (response) {
+        $.each(response, function (index, item) {
+          $('.cards').append('<div class="col s6"><div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="images/work3.jpg"></div><div class="card-content"><span class="card-title activator grey-text text-darken-4" style="line-height: 1.5;"><i class="material-icons right">more_vert</i>' + item.title + '</span><p><br><div class="chip">Tag</div><div class="chip">Tag 2 yo!<!-- <i class="material-icons">close</i> --></div></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>' + item.title + '</span><p>' + item.summary + '</p><p><br><div class="chip">Tag<i class="material-icons">close</i></div><div class="chip">Tag 2 yo!<i class="material-icons">close</i></div></p></div><div class="card-action">' + '<img class="favicon" src="' + 'http://www.google.com/s2/favicons?domain=' + item.site_name + '">' + '<span class="resource-site_name">' + item.site_name + '</span><a href="' + item.url + '" class="resource-original">Open original<i class="material-icons right">open_in_new</i></a></div></div></div>'
         )
       })
+    }, // end success action
+    error: function (xhr, ajaxOptions, thrownError) {
+      // else error, redirect to login
+      window.location.href = 'login.html'
     }
+  }) // end ajax call
+} // end function loadResources
 
+// add new resource
+$(function () {
+  // listen for the add new resource form
+  $('#login-form').on('submit', function (event) {
+    event.preventDefault()
+    var formData = $(this).serialize()
+    login(formData)
   })
+})
+
+function login (formData) {
+  $.ajax({
+    type: 'POST',
+    url: serverURL + 'login',
+    data: formData,
+    success: function (response) {
+      // success save the repsonse
+      window.localStorage.email = $('#email').val()
+      window.localStorage.auth_token = response.auth_token
+      // then redirect
+      window.location.href = 'dashboard.html'
+    },
+    error: function (xhr, ajaxOptions, thrownError) {
+      // else output error
+      console.log(xhr.status)
+      console.log(thrownError)
+      window.alert('Login Failed')
+    }
+  })
+}
 
 }) // end document ready
