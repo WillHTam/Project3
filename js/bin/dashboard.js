2aeb8b8fa2501cc058ccafdf418364dc5397a9d8
Sprazzeus <sprazzeus@gmail.com>
2016-07-21 08:53:14 +0800

    changed order of display to most recent on top (prepend)

--- js/bin/dashboard.js
+++ js/bin/dashboard.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
       success: function (response) {
         console.log('Received resources from server: ' + response)
         $.each(response, function (index, item) {
-          $('.cards').append(
+          $('.cards').prepend(
             '<div class="col s6"><div class="card" id="resource" data-id="' + item._id + '"><div class="card-image waves-effect waves-block waves-light"><img class="activator" style="height: 220px" src="' + item.thumbnail + '"></div>' + '<div class="card-content"><span class="card-title activator grey-text text-darken-4 line-clamp" style="line-height: 1.5;"><i class="material-icons right">more_vert</i>' + item.title + '</span><br><br><div id="outer-tags"></div><div class="chip">' + item.tags[0] + '</div></div><div class="card-reveal"><span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>' + item.title + '</span><p>' + item.description + '</p><br><div class="chip">' + item.tags[0] + '<i class="material-icons" id="delete-tag">close</i></div><br><br><button class="btn waves-effect waves-light" type="submit" name="action" id="submit" data-id="' + item._id + '">Delete<i class="material-icons"  id="delete-resource">delete</i></button></div>' +
             '<div class="card-action">' + '<img class="favicon" src="' + 'https://www.google.com/s2/favicons?domain=' + item.site_name + '">' + '<span class="resource-site_name">' + item.site_name + '</span><a href="' + item.url + '" class="resource-original" target="_blank">Open<i class="material-icons right">open_in_new</i></a></div></div></div>'
           )
