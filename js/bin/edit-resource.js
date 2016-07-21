1f4da509f3244aa95622374fa87eac66a572bba7
Sprazzeus <sprazzeus@gmail.com>
2016-07-21 15:12:25 +0800

    achieved modal on edit button

--- js/bin/edit-resource.js
+++ js/bin/edit-resource.js
@@ -4,6 +4,14 @@ console.log('edit-tag.js loaded')
 $(function listenEdit () {
   // listen for edit button
   $('.resources').on('click', '.edit-resource', function (event) {
+    $('header').append(
+    '<div id="edit-resource-modal" class="modal open" style="z-index: 1003; display: block; opacity: 1; transform: scaleX(1); top: 10%;">' +
+
+    '<div class="modal-content"><h4 class="header"></h4><form id="edit-resource-form" class="col s12" action="https://project3pockety.herokuapp.com/resources" method="post"><div class="row"><div class="input-field col s12"><label for="tags">Tags</label><input type="text" name="tags" id="tags" value data-role="materialtags"></div></div><button class="btn waves-effect waves-light" type="submit" name="action" id="submit">Update<i class="material-icons right">send</i></button></form></div>' + 
+
+    '<div class="modal-footer"><a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Cancel</a></div></div>'
+    )
+
     console.log(this)
     var resource = this;
     console.log(resource)
