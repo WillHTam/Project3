4dfd6f53fe0438be0f3aa68974fea6f2b30ef844
Sprazzeus <sprazzeus@gmail.com>
2016-08-18 17:11:12 +0800

    working on empty tags fix

--- js/bin/dashboard.js
+++ js/bin/dashboard.js
@@ -44,25 +44,25 @@ $(document).ready(function() {
             '<div class="col s4"><div class="card" id="resource" data-id="' + item._id + '"><div class="card-image waves-effect waves-block waves-light"><img class="activator" style="height: 200px" src="' + item.thumbnail + '"></div>' + '<div class="card-content"><span class="card-title activator grey-text text-darken-4 line-clamp" style="line-height: 1.5;"><i class="material-icons right">more_vert</i>' + item.title + '</span><br><br><div id="outer-tags"></div>' + '</div><div class="card-reveal"><span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>' + item.title + '</span><p>' + item.description + '</p><br><div id="inner-tags">' + '</div><br><br><button class="btn waves-effect waves-light edit-resource modal-trigger" href="#edit-resource-modal" type="submit" name="action" id="submit" data-id="' + item._id + '">Edit Tags<i class="material-icons">edit</i></button><br><br><button class="btn waves-effect waves-light delete-resource" type="submit" name="action" id="submit" data-id="' + item._id + '">Delete<i class="material-icons">delete</i></button></div>' +
             '<div class="card-action">' + '<img class="favicon" src="' + 'https://www.google.com/s2/favicons?domain=' + item.site_name + '">' + '<span class="resource-site_name">' + item.site_name + '</span><a href="' + item.url + '" class="resource-original" target="_blank">Open<i class="material-icons right">open_in_new</i></a></div></div></div>'
           )
-          // show tags
-          for (var i = 0; i < item.tags.length; i++) {
-            if (item.tags.length) {
+          // show tags if there are any
+          if (item.tags.length > 0) {
+            for (var i = 0; i < item.tags.length; i++) {
               $('#outer-tags').append('<div class="chip">' + item.tags[i] + '</div>')
               $('#inner-tags').append('<div class="chip">' + item.tags[i] + '</div>')
             }}
-        }) // end loop populating cards
+          }) // end loop populating cards
 
-        $('#masonry-grid').masonry({
-          columnWidth: '.col',
-          itemSelector: '.col',
-        }); // reapply masonry to all content
+          $('#masonry-grid').masonry({
+            columnWidth: '.col',
+            itemSelector: '.col',
+          }); // reapply masonry to all content
 
-      }, // end success action
-      error: function (xhr, ajaxOptions, thrownError) {
-        // else error, redirect to login
-        window.location.href = 'login.html'
-      }
-    }) // end ajax call
-  } // end function loadResources
+        }, // end success action
+        error: function (xhr, ajaxOptions, thrownError) {
+          // else error, redirect to login
+          window.location.href = 'login.html'
+        }
+      }) // end ajax call
+    } // end function loadResources
 
-}) // end document ready
+  }) // end document ready
